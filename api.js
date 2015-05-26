43d50edce05bed860c287cc4ee71254c7b86e228
Brian Wheeler <bwheeler96@gmail.com>
2015-05-26 10:29:22 -0700

    adds ability to set callbacks from controller

--- api.js
+++ api.js
@@ -1,38 +1,52 @@
-(function() {
-  var apiProvider;
+fiestaProvider = function() {
 
-  fiestaProvider = function() {
-    this.$get = ['$http', function($http) {
+  // Callbacks are application specific and start empty
+  callbackNames = ['success', 'error', 'finally'];
+  var callbacks = {};
+  self = this;
 
-      var api, apiParams, apiURL;
+  this.$get = ['$http', function($http) {
 
-      api = function(config, opts) {
-        var promise;
-        opts = angular.extend({
-          useLoadingView: true,
-          debug: window.location.hostname === 'localhost'
-        }, opts);
-        angular.extend(config, {
-          data: apiParams(config.data),
-          params: apiParams(config.params),
-          url: apiURL(config.url)
-        });
-        promise = $http(config).success(function(data, status) {
-          if (opts.debug) {
-            console.log('API: ', config.url, data, status);
-          }
-          return api.successCallback();
-        }).error(function(data, status) {
-          console.error('API ERROR: ', config.method, ' ', config.url, '\nStatus: ', status, '\nData: ', data);
-          return api.errorCallback();
-        });
-        promise["finally"](api.finallyCallback);
-        return promise;
+    var api, apiParams, apiURL;
+
+    api = function(config, opts) {
+      var promise;
+      opts = angular.extend({
+        useLoadingView: true,
+        debug: window.location.hostname === 'localhost'
+      }, opts);
+      angular.extend(config, {
+        data: apiParams(config.data),
+        params: apiParams(config.params),
+        url: apiURL(config.url)
+      });
+
+      promise = $http(config).success(function(data, status) {
+        if (opts.debug) console.log('API: ', config.url, data, status);
+      }).error(function(data, status) {
+        console.error('API ERROR: ', config.method, ' ', config.url, '\nStatus: ', status, '\nData: ', data);
+      });
+
+      angular.forEach(callbackNames, function(cbn) {
+        promise[cbn](callbacks[cbn])
+      });
+
+      return promise;
+    };
+
+    angular.forEach(callbackNames, function(m) {
+      callbacks[m] = function() {};
+      self[m] = function(f) {
+        callbacks[m] = f;
       };
+      api[m] = function(f) {
+        callbacks[m] = f;
+      };
+    });
 
-      angular.forEach(['get', 'post', 'put', 'delete'], function(method) {
-        var config;
-        api[method] = function(url, params, opts) {};
+    angular.forEach(['get', 'post', 'put', 'delete'], function(method) {
+      var config;
+      api[method] = function(url, params, opts) {
         config = {
           method: method.toUpperCase(),
           url: url
@@ -43,35 +57,35 @@
           config.data = params;
         }
         return api(config, opts);
-      });
-
-      api.user = function() {
-        return JSON.parse(localStorage.user);
       };
+    });
 
-      api.setUser = function(u) {
-        return localStorage.user = JSON.stringify(u);
-      };
+    api.user = function() {
+      return JSON.parse(localStorage.user);
+    };
 
-      apiURL = function(path) {
-        while (path.indexOf('/') === 0) {
-          path = path.substr(1);
-        }
-        return 'https://runfiesta.com/v1/' + path;
-      };
+    api.setUser = function(u) {
+      return localStorage.user = JSON.stringify(u);
+    };
 
-      return apiParams = function(params) {
-        params = angular.extend({
-          authentication_token: localStorage.authentication_token
-        }, params);
-        return params;
-      };
+    apiURL = function(path) {
+      while (path.indexOf('/') === 0) {
+        path = path.substr(1);
+      }
+      return 'https://runfiesta.com/v1/' + path;
+    };
 
-    }];
+    apiParams = function(params) {
+      params = angular.extend({
+        authentication_token: localStorage.authentication_token
+      }, params);
+      return params;
+    };
 
     return api
-  };
 
-  angular.module("app", []).provider('fiesta', fiestaProvider);
+  }];
 
-}).call(this);
+};
+
+angular.module('fiesta', []).provider('fiesta', fiestaProvider);
